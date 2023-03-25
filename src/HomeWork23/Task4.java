3e354214c96ee62bc5a45342812628f0265c038c
Farhatyar <farhatyartamim290@gmail.com>
2023-03-25 19:08:08 -0400

    Mar 25 23

---
+++ src/HomeWork23/Task4.java
@@ -0,0 +1,21 @@
+package HomeWork23;
+
+public class Task4 {
+    /*Create a method checkUserName that will throw a runtime exception.
+   Method should throw an exception when entered username is less than 5 characters.
+   */
+    public static void checkUserName(String usreName){
+        if (usreName.length()<5){
+            throw new RuntimeException("Username should be 5 characters or more");
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            checkUserName("Omar");
+        }catch (RuntimeException e) {
+            System.out.println(e);
+        }
+    }
+
+}
